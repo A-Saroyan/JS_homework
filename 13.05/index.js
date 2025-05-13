210533c64c99c36c274344209ed31dda6b3af953
A-Saroyan <andranik.saroyan1@gmail.com>
2025-05-13 16:08:03 +0400

    May 13

--- 13.05/index.js
+++ 13.05/index.js
@@ -1,1 +1,37 @@
-console.log(" 'npm run dev' command is working !!!")
+const express = require("express")
+require('dotenv').config()
+
+const app = express();
+
+const users = [
+    {
+      "name": "Liam",
+      "surname": "Foster",
+      "email": "liam.foster@example.com",
+    },
+    {
+      "name": "Emma",
+      "surname": "Griffin",
+      "email": "emma.griffin@example.com",
+    },
+    {
+        "name": "Ethan",
+        "surname": "Parker",
+        "email": "ethan.parker@example.com",
+    }
+  ]
+
+app.get('/',(req,res)=> {
+    res.send("Welcome to the API.")
+})   
+
+app.get('/users',(req,res)=>{
+
+    res.send(users);
+})
+
+app.listen(process.env.PORT, () => {
+    console.log(`Example app listening on port ${process.env.PORT}`)
+  })
+
+
